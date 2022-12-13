b5bac9d68d767c3303ee36f0388b748e49d262b8
muhammadharuna37 <muhammadharuna37@gmail.com>
2022-12-12 18:47:52 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,16 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *
- *  * main - Entry                                                                                                                         
- *
- *   * Return: Always 0                                                                                                                     
- *
- *    */                                                                                                                                     
+#include <stdio.h>
 
-int main(void)                                                                                                                          
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
-{                                                                                                                                       
+int main(void)
 
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
+{
+		puts("\"Programming is like building a multilingual puzzle");
 
-			        return (0);
-
+					return (0);
 }
+
+
