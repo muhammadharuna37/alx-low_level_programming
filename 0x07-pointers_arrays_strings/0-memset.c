50c9bccd4b295156193acd4696a0cbd051e61554
Muhammad Haruna <muhammadharunagit add .lsrootcd alx-zero_daygit config --global user.email muhammadharuna>
2023-01-02 13:29:10 -0800

    task0 git add . git commit -m task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,13 @@
 /**
  *  _memset - fills memory with a constant byte.
  *  @s: source string
- *  @b: the constant filling
+ *  @b: the constant byte for filling
  *  @n: lenght of buffer
  *  Return: new string.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	
 	while (i < n)
 	{
 		*(s + i) = b;
